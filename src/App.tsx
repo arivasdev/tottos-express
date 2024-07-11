ac5a22e301cdd2aa62b71c5ab7f859f6043af917
Gerardo Valle <gavalle96@gmail.com>
2024-07-10 19:04:36 -0600

    Sidebar navigation and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './pages/Home';
 import Settings from './pages/Settings';
-import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+// import Users from './pages/settings/Users';
+// import Roles from './pages/settings/Roles';
+// import Categories from './pages/settings/Categories';
+// import DeliveryMethods from './pages/settings/DeliveryMethods';
 
-
-function App() {
-
+const App: React.FC = () => {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/settings" element={<Settings />} />
+        {/* <Route path="/settings/users" element={<Users />} />
+        <Route path="/settings/roles" element={<Roles />} />
+        <Route path="/settings/categories" element={<Categories />} />
+        <Route path="/settings/delivery-methods" element={<DeliveryMethods />} /> */}
       </Routes>
     </Layout>
-  )
-}
+  );
+};
 
-export default App
+export default App;
