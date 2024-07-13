5e8a8b060866c7ebd7161f5d5ff9888ef19aa82b
gavalle96 <gavalle96@gmail.com>
2024-07-12 21:56:06 -0600

    Manejo categorias (#4)
    * Manejo de Categorías (Funciona, pero con diseño feo)
    
    * Manejo de categorias en paneles side by side
    
    * Codigo funciona. ToDo: update subCategory
    
    * Mejoras de diseño en categorias
    
    * Gestión de categorías: completo

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,7 +7,7 @@ const Sidebar: React.FC = () => {
 
 
     return (
-        <div className="bg-gray-800 text-white w-64 h-full flex flex-col">
+        <div className="bg-gray-800 text-white w-50 h-full flex flex-col">
             <div className="p-4">
                 <h1 className="text-2xl font-bold">Admin Panel</h1>
             </div>
@@ -27,7 +27,7 @@ const Sidebar: React.FC = () => {
                             <Link to="/settings/users">Usuarios y Permisos</Link>
                         </li>
                         <li className="p-2 hover:bg-gray-700">
-                            <Link to="/settings/categories">Categorías de Productos</Link>
+                            <Link to="/categories/categories">Categorías de Productos</Link>
                         </li>
                         <li className="p-2 hover:bg-gray-700">
                             <Link to="/settings/delivery-routes">Rutas de Entrega</Link>
---
+++ src/pages/categories/SubCategoryForm.tsx
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from 'react';
+import { supabase } from '../../supabaseClient';
+import { PostgrestError } from '@supabase/supabase-js';
+
+interface SubCategoryFormProps {
+    categoryId: string;
+    onClose: () => void;
+    setSubCategories: React.Dispatch<React.SetStateAction<SubCategory[]>>;
+    subCategory: SubCategory|null;
+}
+
+const SubCategoryForm: React.FC<SubCategoryFormProps> = ({ categoryId, onClose, setSubCategories, subCategory }) => {
+    const [subCategoryName, setSubCategoryName] = useState(subCategory?.name || '');
+    const [tarifa, setTarifa] = useState<string>(subCategory?.tarifa || '0.00');
+    const [isCobroPorPeso, setIsCobroPorPeso] = useState(true);
+
+    const handleTarifaChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        // Permitir solo números y un máximo de dos decimales
+        if (/^\d*\.?\d{0,2}$/.test(value)) {
+            setTarifa(value);
+        }
+    };
+
+    useEffect(() => {
+        if(subCategory){
+            setIsCobroPorPeso(subCategory?.isCobroPorPeso);
+        }
+    },[])
+
+    const addSubCategory = async () => {
+        const tarifaValue = parseFloat(tarifa);
+        if (subCategoryName.trim() && tarifaValue >= 0) {
+            let operationError : PostgrestError | null;
+            if(subCategory){
+                const { error } = await supabase.from('SubCategories').update({
+                    name: subCategoryName,
+                    categoryId,
+                    isCobroPorPeso,
+                    tarifa: tarifaValue,
+                    isActive: true,
+                })
+                .eq('id', subCategory.id)
+                ;
+                operationError = error;
+            }
+            else{
+                const { error } = await supabase.from('SubCategories').insert({
+                    name: subCategoryName,
+                    categoryId,
+                    isCobroPorPeso,
+                    tarifa: tarifaValue,
+                    isActive: true,
+                });
+                operationError = error;
+            }
+            
+
+            if (operationError) {
+                console.error('Error saving subcategory:', operationError);
+            } else {
+                setSubCategoryName('');
+                setTarifa('0.00');
+                setIsCobroPorPeso(true);
+                const { data, error: fetchError } = await supabase.from<SubCategory>('SubCategories').select('*').eq('categoryId', categoryId);
+                if (fetchError) {
+                    console.error('Error fetching subcategories:', fetchError);
+                } else {
+                    setSubCategories(data || []);
+                    onClose();
+                }
+            }
+        }
+    };
+
+    return (
+        <div>
+            <label className="block text-sm font-medium text-gray-700 mb-2">Nombre de la Subcategoría</label>
+            <input
+                type="text"
+                value={subCategoryName}
+                onChange={(e) => setSubCategoryName(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded mb-4"
+            />
+
+            <label className="block text-sm font-medium text-gray-700 mb-2">Tarifa (USD)</label>
+            <input
+                type="text"
+                value={tarifa}
+                onChange={handleTarifaChange}
+                className="w-full p-2 border border-gray-300 rounded mb-4"
+            />
+
+            <div className="flex items-center mb-4">
+                <label className="block text-sm font-medium text-gray-700 mr-4">Cobro Por Peso</label>
+                <input
+                    type="checkbox"
+                    id="toggleCobroPorPeso"
+                    className="hidden"
+                    checked={isCobroPorPeso}
+                    onChange={(e) => setIsCobroPorPeso(e.target.checked)}
+                />
+                <label htmlFor="toggleCobroPorPeso" className="flex items-center cursor-pointer ml-2">
+                    <div className={`block ${isCobroPorPeso ? 'bg-green-500' : 'bg-gray-600'} w-14 h-8 rounded-full relative`}>
+                        <div
+                            className={`dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition ${isCobroPorPeso ? 'transform translate-x-full bg-green-500' : ''
+                                }`}
+                        ></div>
+                    </div>
+                </label>
+            </div>
+
+            <button
+                onClick={addSubCategory}
+                className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 focus:outline-none focus:bg-green-600"
+            >
+                { subCategory ? "Guardar Cambios" : "Agregar Subcategoría"}
+            </button>
+        </div>
+    );
+};
+
+export default SubCategoryForm;
